dd1064ee63a2f80ea45d052aa5d59f47430c7de1
Luis Eduardo Goncalves de Macedo <designs.eduardog@gmail.com>
2025-03-05 15:06:46 -0300

    First commit

---
+++ src/main/java/org/tclxedu17/agredadordeinvestimentos/AgredadordeinvestimentosApplication.java
@@ -0,0 +1,14 @@
+package org.tclxedu17.agredadordeinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgredadordeinvestimentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgredadordeinvestimentosApplication.class,
+                args);
+    }
+
+}
---
+++ src/test/java/org/tclxedu17/agredadordeinvestimentos/AgredadordeinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tclxedu17.agredadordeinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgredadordeinvestimentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
