06bdca44f2e889b17578575144d555aa68c8f8e6
Luis Eduardo Goncalves de Macedo <designs.eduardog@gmail.com>
2025-03-05 16:32:16 -0300

    testeCommit

--- src/test/java/org/tclxedu17/agredadordeinvestimentos/AgredadordeinvestimentosApplicationTests.java
+++ src/test/java/org/tclxedu17/agredadordeinvestimentos/AgredadordeinvestimentosApplicationTests.java
@@ -5,11 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
+
 class AgredadordeinvestimentosApplicationTests {
 
     @Test
     void contextLoads( )
-    {
-    }
+    {}
 
 }
